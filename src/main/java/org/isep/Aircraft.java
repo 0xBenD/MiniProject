65ac5fa21527f325805f5797058eec090f5c9829
Ben <74008687+0xBenD@users.noreply.github.com>
2025-11-28 16:26:56 +0100

    First Push
    Aircraft
    flight (debut)
    Airport (début)

--- src/main/java/org/isep/Aircraft.java
+++ src/main/java/org/isep/Aircraft.java
@@ -1,4 +1,29 @@
 package org.isep;
 
+import java.util.ArrayList;
+
 public class Aircraft {
+    private int capacity;
+    private String model;
+    private String registration;
+
+    public Aircraft(int capacity, String model, String registration) {
+        this.capacity = capacity;
+        this.model = model;
+        this.registration = registration;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public boolean verifyCapacity(int passengers){
+        if (passengers <= capacity) return true;
+        else return false;
+    }
+
+    public ArrayList<String> affectFlight(Flight flight){
+        ArrayList<String> passengers = new ArrayList<>();
+        return passengers;
+    }
 }
--- src/main/java/org/isep/Book.java
+++ src/main/java/org/isep/Book.java
@@ -1,4 +1,26 @@
 package org.isep;
+import java.time.Instant;
+import java.util.Date;
 
 public class Book {
+    private int bookNumber;
+    private String Status;
+    private Date date;
+
+    public Book(int bookNumber){
+        this.bookNumber = bookNumber;
+        this.date = Date.from(Instant.now());
+    }
+
+    public boolean confirmReservation(Flight flight, int ID,String passeport){
+
+    }
+
+    public void bookCancellation(){
+
+    }
+    public void bookModification(){
+
+    }
+
 }
