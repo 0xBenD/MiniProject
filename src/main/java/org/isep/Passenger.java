cf2fb3d5ef7f2de69ae6c7f6f87652eca1fb3cd5
Sanytol-tout-dans-la-maison <sany.delest@eleve.isep.fr>
2025-11-28 16:43:04 +0100

    Update Passenger 2
    Updated the methods and added a Book Array List

--- src/main/java/org/isep/Passenger.java
+++ src/main/java/org/isep/Passenger.java
@@ -1,22 +1,40 @@
 package org.isep;
 
+import java.util.ArrayList;
+
 public class Passenger extends Person{
 
     private String passeport;
+    private ArrayList<Book> bookArrayList = new ArrayList<>();
 
     public Passenger(String name, String address, String contact, String passeport){
         super(name, address, contact);
         this.passeport = passeport;
     }
 
-    public void bookFlight(Flight flight){
-        Book book = new Book();
-        boolean success = book.confirmReservation(flight, super.getID(), passeport);
-        if (success) System.out.printf("Booked flight number %d successfully.\n", flightNumber);
-        else System.out.printf("Failed to book flight number %d.\n", flightNumber);
-    }
-
     public String getPasseport() {
         return passeport;
     }
+
+    public void bookFlight(Flight flight){
+        Book book = new Book();
+        bookArrayList.add(book);
+        boolean success = book.confirmReservation(flight, super.getID(), passeport);
+        if (success) System.out.printf("Booked flight number %d successfully.\n", flight.getFlightNumber);
+        else System.out.printf("Failed to book flight number %d.\n", flight.getFlightNumber);
+    }
+
+    public void cancelFlight(int bookNumber){
+        for(book b : bookArrayList){
+            if(b.getReservationNumber == bookNumber) b.cancelReservation();
+        }
+    }
+
+    public void getReservations(int bookNumber){
+        for(book b : bookArrayList){
+            if(b.getReservationNumber == bookNumber) System.out.printf("Reservation number is : %d , the date is : %s , status : ", bookNumber, b.getDate, b.getReservationNumber);
+        }
+    }
+
+
 }
