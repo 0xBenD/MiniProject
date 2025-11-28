672af00ecb0f93106c7410a40eee262bae311f8f
Sanytol-tout-dans-la-maison <sany.delest@eleve.isep.fr>
2025-11-28 15:29:43 +0100

    Update Person.java

--- src/main/java/org/isep/Person.java
+++ src/main/java/org/isep/Person.java
@@ -1,4 +1,35 @@
 package org.isep;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Person {
+    private static ArrayList<Integer> IDList = new ArrayList<>();
+    private int ID;
+    private String name;
+    private String address;
+    private String contact;
+
+    public Person(String name, String address, String contact){
+        this.name = name;
+        this.address = address;
+        this.contact = contact;
+        Random randomInt = new Random();
+        boolean cond;
+        int newID;
+        do {
+            cond = false;
+            newID = randomInt.nextInt();
+            for (int i = 0; i < IDList.size(); i++) {
+                if(IDList.get(i) == newID) cond = true;
+            }
+        } while(cond);
+        IDList.add(newID);
+        this.ID = newID;
+    }
+
+    public void getInfos(){
+        System.out.println("ID : " + ID + "\nname : " + name + "\naddress: " + address + "\n contact : " + contact);
+    }
+
 }
