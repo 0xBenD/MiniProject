be5f5fe75661a3ca8be50b11f3d1eafc79587cdc
Ben <74008687+0xBenD@users.noreply.github.com>
2025-11-29 13:28:39 +0100

    correction, variables Airport
    correction des variables.
    prochaine maj a suivre pour la fin de Airport.java

--- src/main/java/org/isep/Airport.java
+++ src/main/java/org/isep/Airport.java
@@ -1,17 +1,21 @@
 package org.isep;
 
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Airport{
     private String name;
     private String city;
     private String description;
+    private ArrayList<Flight> departingFlights = new ArrayList<>();
+    private ArrayList<Flight> arrivingFlights = new ArrayList<>();
+
+
     public Airport(String name, String city, String description){
         this.name = name;
         this.city = city ;
         this.description = description;
     }
-
-    public void affetFlight(){}
-
 
 }
