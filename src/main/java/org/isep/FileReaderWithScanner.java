82b213616f9d55344d4920b871df3a211f634ff8
Sanytol-tout-dans-la-maison <sany.delest@eleve.isep.fr>
2025-12-05 15:07:05 +0100

    Create FileReaderWithScanner.java
    Implements readCSV method

---
+++ src/main/java/org/isep/FileReaderWithScanner.java
@@ -0,0 +1,23 @@
+package org.isep;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+
+
+public class FileReaderWithScanner {
+
+
+    public static ArrayList<String> readCSV(String fileName, ArrayList<String> data) throws IOException{
+
+        String file = fileName;
+        Scanner scanner = new Scanner(new File(file));
+        scanner.useDelimiter(",");
+        while(scanner.hasNext()){
+            data.add(scanner.next());
+        }
+        scanner.close();
+        return data;
+    }
+}
+
