025c38bc9b48f6e5dda8c43ff8155e89c20beb74
benjamin Dinon <benjamindinon@MacBook-Air-de-benjamin.local>
2025-12-05 15:22:36 +0100

    test

--- src/main/java/org/isep/Main.java
+++ src/main/java/org/isep/Main.java
@@ -6,6 +6,6 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        ArrayList<String> data = FileReaderWithScanner.readCSV("flight.txt", new ArrayList<String>());
+        ArrayList<String> data = FileReaderWithScanner.readCSV("src/main/resources/flight.txt", new ArrayList<String>());
     }
 }
