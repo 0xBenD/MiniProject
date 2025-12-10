45bfecaa4a762a855ea3a219533519245230757f
benjamin Dinon <benjamin.dinon@icloud.com>
2025-12-10 17:32:47 +0100

    addAircraft + userRemoveAircraft

--- src/main/java/org/isep/Aircraft.java
+++ src/main/java/org/isep/Aircraft.java
@@ -1,11 +1,16 @@
 package org.isep;
-
+import org.isep.FileClasses.WriteToFile;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.time.LocalDateTime;
 
 public class Aircraft {
 
     private static ArrayList<Aircraft> fleet = new ArrayList<>();
+    private static final String PATHNAME = "src/main/resources/aircraft.csv";
 
     private int capacity;
     private String model;
@@ -53,6 +58,52 @@ public class Aircraft {
         if(schedule.contains(flight)){
             schedule.remove(flight);
             System.out.println("Aircraft "+registration+" has been released");
+        }
+    }
+
+    public String toCSV(){
+        return capacity + "," +model + "," + registration + "\n";
+    }
+    public static void updateAircraftCSV() throws IOException{
+        File file = new File(PATHNAME);
+        FileWriter fw = new FileWriter(file, false);
+        for(Aircraft a : fleet){
+            WriteToFile.write(PATHNAME, a.toCSV());
+        }
+    }
+    public static void addAircraft(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter Aircraft's capacity : ");
+        int capacity = Integer.parseInt(scanner.nextLine());
+        System.out.println("Please enter Aircraft's model : ");
+        String model = scanner.nextLine();
+        System.out.println("Please enter Aircraft's registration : ");
+        String registration = scanner.nextLine();
+        new Aircraft(capacity, model, registration);
+        System.out.println("Aircraft added successfully\n");
+        try{
+            updateAircraftCSV();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public static void userRemoveAircraft(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Registration to Remove : ");
+        String registration = scanner.nextLine();
+        Aircraft a = findAircraft(registration);
+        if(a != null){
+            fleet.remove(a);
+            System.out.println("Aircraft removed");
+            try{
+                updateAircraftCSV();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        else {
+            System.out.println("Aircraft not found");
         }
     }
 
