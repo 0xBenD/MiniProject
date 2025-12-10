cb22c4d2457eacaeaf799895bd29434206e8bc88
benjamin Dinon <benjamin.dinon@icloud.com>
2025-12-10 19:22:02 +0100

    Update StaffCabin.java

--- src/main/java/org/isep/StaffCabin.java
+++ src/main/java/org/isep/StaffCabin.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class StaffCabin extends Employee{
 
@@ -49,6 +50,59 @@ public class StaffCabin extends Employee{
         File file = new File(PATHNAME);
         FileWriter fw = new FileWriter(file, false);
         for(StaffCabin a : allCabinStaff) WriteToFile.write(PATHNAME, a.toCSV());
+    }
+
+    public static void addStaffCabin(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter Staff's full name");
+        String name = scanner.nextLine();
+        System.out.println("Please enter address");
+        String address = scanner.nextLine();
+        System.out.println("Please enter contact");
+        String contact = scanner.nextLine();
+        System.out.println("Please enter Hiring Date");
+        String date = scanner.nextLine();
+        System.out.println("Please enter Qualification");
+        String qualif = scanner.nextLine();
+
+        new StaffCabin(name, address, contact, date, qualif);
+        System.out.println("Staff added successfully\n");
+    }
+
+    public static void userRemoveStaffCabin() {
+        Scanner scanner = new Scanner(System.in);
+        String licenceStr = "";
+        int idToRemove = 0;
+        boolean cond = false;
+
+        for (StaffCabin p : allCabinStaff) System.out.println("Staff: " + p);
+        while (!cond) {
+            System.out.println("Please enter Employee ID to remove");
+            try {
+                idToRemove = Integer.parseInt(scanner.nextLine());
+                // On vérifie si l'ID existe
+                for (StaffCabin s : allCabinStaff) {
+                    if (s.getNumberEmp() == idToRemove) cond = true;
+                }
+                if (!cond) System.out.println("Enter an existing Employee ID");
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number");
+            }
+        }
+
+        System.out.println("Removing Staff with ID " + idToRemove);
+        StaffCabin toRemove = null;
+        for (StaffCabin s : allCabinStaff) {
+            if (s.getNumberEmp() == idToRemove) toRemove = s;
+        }
+        if (toRemove != null) {
+            allCabinStaff.remove(toRemove);
+            System.out.println("Staff deleted");
+            try {
+                updateStaffCabinCSV();
+            } catch (IOException e) {
+            }
+        }
     }
 
     @Override
