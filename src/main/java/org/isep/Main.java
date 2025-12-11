a8f6430fc1201bec92e7b16505f40a1669e59cbb
benjamin Dinon <benjamin.dinon@icloud.com>
2025-12-11 16:11:53 +0100

    création du menue

--- src/main/java/org/isep/Main.java
+++ src/main/java/org/isep/Main.java
@@ -17,6 +17,7 @@ public class Main {
     //public static LocalDateTime simulationTime = LocalDateTime.of(2023,12,10,9,0 );
 
     public static void main(String[] args) throws IOException {
+
         readAlldata();
         testReadData();
         assignAircraftTest();
@@ -26,8 +27,125 @@ public class Main {
         updateAllData();
         updateFlightStatus();
 
+
+
+        interractiveMenue();
+
         for(Book book : Book.getAllBooks()) System.out.println(book);
         displayStats();
+    }
+
+    public static void interractiveMenue(){
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+        System.out.println("#######################################################################################################################################################################################################");
+        System.out.println("                                                                             AIRLINE MANAGEMENT SYSTEM CONSOLE"                                                                                         );
+        System.out.println("#######################################################################################################################################################################################################");
+        while(running){
+            System.out.println("\n -- Main Menue -- \n");
+            System.out.println("1.  Manage Passengers (Add/Remove/Edit)");
+            System.out.println("2.  Manage Flights (Add/Remove/Edit)");
+            System.out.println("3.  Manage Aircrafts & Airports");
+            System.out.println("4.  Manage Staff (Pilots & Cabin)");
+            System.out.println("5.  SIMULATION: Advance Time (Update Status)");
+            System.out.println("6.  SIMULATION: Re-Assign Aircrafts (Random)");
+            System.out.println("7.  Display Global Statistics");
+            System.out.println("0.  Save Data & Exit");
+            System.out.print("Enter your choice: ");
+            String choice = scanner.nextLine();
+            switch(choice) {
+                case "1":
+                    System.out.println("\n--- PASSENGERS ---");
+                    System.out.println("1. Add Passenger");
+                    System.out.println("2. Remove Passenger");
+                    System.out.println("3. Edit Passenger");
+                    System.out.println("4. List All Passengers");
+                    System.out.print("Choice: ");
+                    String pChoice = scanner.nextLine();
+                    if (pChoice.equals("1")) Passenger.addPassenger();
+                    else if (pChoice.equals("2")) Passenger.userRemovePassenger();
+                    else if (pChoice.equals("3")) Passenger.userEditPassenger();
+                    else if (pChoice.equals("4")) Passenger.listAllPassengers();
+                    break;
+
+
+                case "2":
+                    System.out.println("\n--- FLIGHTS ---");
+                    System.out.println("1. Add Flight");
+                    System.out.println("2. Remove Flight");
+                    System.out.println("3. Edit Flight");
+                    System.out.println("4. List All Flights");
+                    System.out.print("Choice: ");
+                    String fChoice = scanner.nextLine();
+                    if(fChoice.equals("1")) Flight.addFlight();
+                    else if(fChoice.equals("2")) Flight.userRemoveFlight();
+                    else if(fChoice.equals("3")) Flight.userEditFlight();
+                    else if(fChoice.equals("4")) {
+                        for(Flight f : Flight.getAllFlights()) System.out.println(f);
+                    }
+                    break;
+
+                case "3":
+                    System.out.println("\n--- INFRASTRUCTURE ---");
+                    System.out.println("1. Add Aircraft");
+                    System.out.println("2. Remove Aircraft");
+                    System.out.println("3. Edit Aircraft");
+                    System.out.println("4. Add Airport");
+                    System.out.println("5. Remove Airport");
+                    System.out.print("Choice: ");
+                    String iChoice = scanner.nextLine();
+                    if(iChoice.equals("1")) Aircraft.addAircraft();
+                    else if(iChoice.equals("2")) Aircraft.userRemoveAircraft();
+                    else if(iChoice.equals("3")) Aircraft.userEditAircraft();
+                    else if(iChoice.equals("4")) Airport.addAirport();
+                    else if(iChoice.equals("5")) Airport.userRemoveAirport();
+                    break;
+
+                case "4":
+                    System.out.println("\n--- STAFF ---");
+                    System.out.println("1. Add Pilot");
+                    System.out.println("2. Remove Pilot");
+                    System.out.println("3. Edit Pilot");
+                    System.out.println("4. Add Cabin Staff");
+                    System.out.println("5. Remove Cabin Staff");
+                    System.out.print("Choice: ");
+                    String sChoice = scanner.nextLine();
+                    if(sChoice.equals("1")) AirplanePilot.addPilot();
+                    else if(sChoice.equals("2")) AirplanePilot.userRemovePilot();
+                    else if(sChoice.equals("3")) AirplanePilot.userEditPilot();
+                    else if(sChoice.equals("4")) StaffCabin.addStaffCabin();
+                    else if(sChoice.equals("5")) StaffCabin.userRemoveStaffCabin();
+                    break;
+
+                case "5":
+                    System.out.println("How many minutes do you want to advance?");
+                    try {
+                        int minutes = Integer.parseInt(scanner.nextLine());
+                        userAdvanceTime(minutes);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid number.");
+                    }
+                    break;
+
+                case "6":
+                    assignAircraftTest();
+                    break;
+
+                case "7":
+                    displayStats();
+                    break;
+
+                case "0":
+                    System.out.println("Saving data...");
+                    updateAllData();
+                    running = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid option. Please try again.");
+
+            }
+        }
     }
 
     public static void readAlldata() throws IOException {
